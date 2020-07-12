49a1fe963a8519438dc16a7ebaa32bcc1c12773d
littleaj <1690572+littleaj@users.noreply.github.com>
2020-07-12 12:37:03 -0700

    move server code to ts

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const gtsc = require('gulp-typescript');
 
 const SRC_DIR = './src';
 const SRC_NODE = SRC_DIR+'/*.js';
-const SRC_JS = SRC_DIR + '/scripts/**/*.js';
 const SRC_CSS = SRC_DIR + '/css/*.css';
 const SRC_SVG = SRC_DIR + '/images/*.svg';
 const SRC_PNG = SRC_DIR + '/images/*.png';
@@ -20,6 +19,9 @@ const WWW_CSS = WWW_ROOT + '/css';
 const WWW_IMAGES = WWW_ROOT + '/images';
 const WWW_IMAGES_BG = WWW_IMAGES + '/bg';
 const WWW_JSON_DATA = WWW_ROOT + '/json';
+const TS_OUTPUT = './built';
+const WEB_JS = TS_OUTPUT + '/web/**/*.js';
+const SERVER_JS = TS_OUTPUT + '/server/**/*.js';
 
 function copyFiles(srcPattern, destDir) {
     console.log("Deploying "+srcPattern+" into "+destDir);
@@ -35,22 +37,22 @@ async function info() {
     console.log("ts:   ",tsProject.config.include);
 }
 
-function node() {
-    return copyFiles(SRC_NODE, OUTPUT_DIR);
-}
-
 function html() {
     return copyFiles(SRC_HTML, WWW_ROOT);
-}
-
-function js() {
-    return copyFiles(SRC_JS, WWW_JS);
 }
 
 
 function ts() {
     var tsResult = tsProject.src().pipe(tsProject());
-    return tsResult.js.pipe(gulp.dest(WWW_JS));
+    return tsResult.js.pipe(gulp.dest(TS_OUTPUT));
+}
+
+async function webJs() {
+    return gulp.src(WEB_JS).pipe(gulp.dest(WWW_JS));
+}
+
+async function serverJs() {
+    return gulp.src(SERVER_JS).pipe(gulp.dest(OUTPUT_DIR));
 }
 
 async function jsdeps() {
@@ -84,9 +86,14 @@ function json() {
     return copyFiles(SRC_JSON, WWW_JSON_DATA);
 }
 
-async function clean() {
+async function cleanDist() {
     console.log("Cleaning "+OUTPUT_DIR);
     return del.sync(OUTPUT_DIR);
+}
+
+async function cleanBuilt() {
+    console.log("Cleaning "+TS_OUTPUT);
+    return del.sync(TS_OUTPUT);
 }
 
 function watch() {
@@ -100,13 +107,14 @@ function watch() {
     gulp.watch(SRC_JSON, json);
 }
 
-exports.clean = clean;
+exports.clean = gulp.series(cleanDist, cleanBuilt);
 exports.watch = watch;
-exports.build = gulp.series(html, json, jsdeps, ts, css, images, node);
+exports.build = gulp.series(html, json, jsdeps, ts, css, images, webJs, serverJs);
 exports.html = html;
 exports.css = css;
 exports.images = images;
-exports.node = node;
-exports.ts = gulp.series(jsdeps, ts);
+exports.nodejs = gulp.series(ts, serverJs);
+exports.ts = gulp.series(jsdeps, ts, webJs, serverJs);
+exports.webjs = gulp.series(jsdeps, ts, webJs);
 exports.info = info;
 exports.json = json;
