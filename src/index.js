e6ff674d2e28752c6d66da480f4c24aee629cf18
littleaj <1690572+littleaj@users.noreply.github.com>
2020-07-12 12:00:13 -0700

    serve json files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let app = express();
 const PORT = 8888;
 
 app.use(express.static(path.join(__dirname, 'site')));
-app.use(express.static(path.join(__dirname, 'site', 'json')))
+app.use(express.static(path.join(__dirname, 'site', 'json')));
 
 let server = app.listen(PORT, function () {
     console.log("Started app: %o", server.address());
