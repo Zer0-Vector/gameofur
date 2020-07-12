c7d13834f39b182b071083f5c02c07e7cd5e5c9c
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2020-07-12 12:37:03 -0700

    move server code to ts

--- src/ts/server/index.ts
+++ src/ts/server/index.ts
@@ -2,13 +2,12 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-
 const PORT = 8888;
 
 app.use(express.static(path.join(__dirname, 'site')));
 app.use(express.static(path.join(__dirname, 'site', 'json')));
 
-let server = app.listen(PORT, function () {
+let server = app.listen(PORT, () => {
     console.log("Started app: %o", server.address());
 });
 console.log('Started server');
