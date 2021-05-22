f0c0140f483426de88478d19c73945cd0d4ddde9
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2021-05-21 20:02:45 -0700

    add dice control buttons. rework some layout of dice and buttons

--- gameofur-react/src/view/components/DiceControlsComponent.tsx
+++ gameofur-react/src/view/components/DiceControlsComponent.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { ButtonComponent } from "./ButtonComponent";
+import './DiceControls.css'
 
 export class DiceControlsComponent extends React.Component {
     render() {
         return (
             <div id="diceControls">
-                
+                <ButtonComponent id="roll" displayText="&Roll" />
+                <ButtonComponent id="pass" displayText="&Pass Turn" />
             </div>
         );
     }
