1083307127458029e4d6a08643db6e261c9e72f2
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-01-16 18:48:20 -0800

    add pieces to gameContext

--- src/model/GameContext.js
+++ src/model/GameContext.js
@@ -1,5 +1,25 @@
 import React from "react"
+import Player from "./Player"
 
-const GameContext = React.createContext({})
+const GameContext = (() => {
+    const p1 = new Player(1)
+    const p2 = new Player(2)
+    const players = [p1,p2]
+    const pieceCount = 7;
+    var pieces = new Array(pieceCount * players.length)
+    for (var i = 0; i < players.length; i++) {
+      for (var j = 0; j < pieceCount; j++) {
+        pieces.push(new PieceData(i, j))
+      }
+    }
+
+    return React.createContext(
+      {
+        players: players,
+        pieces: pieces,
+      }
+    )
+  }
+)()
 
 export default GameContext
---
+++ src/model/PieceData.js
@@ -0,0 +1,8 @@
+class PieceData {
+  constructor(playerId, pieceId) {
+    this.playerId = playerId
+    this.id = pieceId
+  }
+}
+
+export default PieceData
