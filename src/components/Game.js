c3c3c24664dbf65b4c4789daca96aaac9c107a3e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-01-23 09:53:49 -0800

    convert Game to functional component. Remove dev border. tweak spacing between board and player area.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,30 +1,29 @@
-import React from "react"
+import React, { useContext } from "react"
 import Board from './Board'
 import PlayerArea from './PlayerArea'
 import GameContext from "../model/GameContext"
 import Box from '../containers/Box'
 import './Game.css'
 
-class Game extends React.Component {
-  static contextType = GameContext
+function Game() {
 
-  piecesForPlayer(id) {
-    return this.context.pieces.filter((p,i)=>(p.player.number===id))
+  const context = useContext(GameContext)
+
+  function piecesForPlayer(id) {
+    return context.pieces.filter((p,i)=>(p.player.number===id))
   }
 
-  render() {
-    return (
-      <Box className="game">
-          <Box className="player-area-container">
-            <PlayerArea player={this.context.players[0]} pieces={this.piecesForPlayer(1)} />
-          </Box>
-          <Board />
-          <Box className="player-area-container">
-            <PlayerArea player={this.context.players[1]} pieces={this.piecesForPlayer(2)} />
-          </Box>
-      </Box>
-    )
-  }
+  return (
+    <Box className="game">
+        <Box className="player-area-container">
+          <PlayerArea player={context.players[0]} pieces={piecesForPlayer(1)} />
+        </Box>
+        <Board />
+        <Box className="player-area-container">
+          <PlayerArea player={context.players[1]} pieces={piecesForPlayer(2)} />
+        </Box>
+    </Box>
+  )
 }
 
 export default Game
