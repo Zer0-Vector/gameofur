d1ed8b072c226b74882b9dc98896475c875a686c
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-01-23 10:03:43 -0800

    Add container div for avatar image. adjust container display and tweak size and spacing.

--- src/components/NameBox.js
+++ src/components/NameBox.js
@@ -7,12 +7,14 @@ class NameBox extends React.Component {
     const { player } = this.props
     return (
       <div className='name-box'>
-        <img
-          src={"https://robohash.org/" + player.avatarId() + ".png?bgset=bg2&set=set1"}
-          className="avatar"
-          alt={player.name + " Avatar"}
-          title={player.name + " Avatar"}
-        />
+        <div className='avatar-container'>
+          <img
+            src={"https://robohash.org/" + player.avatarId() + ".png?bgset=bg2&set=set1"}
+            className="avatar"
+            alt={player.name + " Avatar"}
+            title={player.name + " Avatar"}
+          />
+        </div>
         <h2>{player.name}</h2>
       </div>
     )
