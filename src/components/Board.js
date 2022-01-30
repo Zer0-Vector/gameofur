84164b6b4c94d6de86121c4001afb293a29c32fc
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-01-30 14:14:40 -0800

    Update board to use SpaceDropContainers and Gaps

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,9 +4,11 @@ import Space from './Space'
 import Column from '../containers/Column'
 import Box from '../containers/Box'
 import GameContext from '../model/GameContext'
+import Gap from './Gap'
+import SpaceDropContainer from './SpaceDropContainer'
 
 function Board() {
-  const gameContext = useContext(GameContext)
+  const [gameContext,] = useContext(GameContext)
 
   const spaces = new Array(3)
   for (var i = 0; i < 3; i++) {
@@ -15,16 +17,16 @@ function Board() {
       gameContext.spaces
           .filter(item => item.column === index && item.row >= 0)
           .sort((a, b)=> a.row - b.row)
-          .map(item => <Space {...item} key={item.coords()} />)
+          .map(item => <SpaceDropContainer key={item.coords}><Space {...item} /></SpaceDropContainer>)
     )()
   }
   spaces[0].splice(4, 0, 
-    <Space gap={true} edgeClass='right top' key='gap0' />,
-    <Space gap={true} edgeClass='right bottom' key='gap1' />
+    <Gap edgeClass='right top' key='gap0' />,
+    <Gap edgeClass='right bottom' key='gap1' />
   )
   spaces[2].splice(4, 0,
-    <Space gap={true} edgeClass='top left' key='gap2' />,
-    <Space gap={true} edgeClass='bottom left' key='gap3' />
+    <Gap edgeClass='top left' key='gap2' />,
+    <Gap edgeClass='bottom left' key='gap3' />
   )
 
   const wrapWithColumn = (space, index) => <Column key={`col${index}`}>{space}</Column>
