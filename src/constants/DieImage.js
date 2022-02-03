dcc44a9bb25f238f215fa07dcb54778ea22b2bbd
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-03 09:43:08 -0800

    Update DieImage to return components

--- src/constants/DieImage.js
+++ src/constants/DieImage.js
@@ -1,11 +1,16 @@
+import React from 'react'
 import BaseImage from './BaseImage'
+import {ReactComponent as Die0} from '../images/die0.svg'
+import {ReactComponent as Die1} from '../images/die1.svg'
 
 export default class DieImage extends BaseImage {
 
-  static die0 = new DieImage('die0')
-  static die1 = new DieImage('die1')
-
-  constructor(imageName) {
-    super('img-', imageName)
+  static getImage(pips) {
+    switch (pips) {
+      case 0:
+        return <Die0 className="die0" />
+      case 1:
+        return <Die1 className="die1" />
+    }
   }
 }
