0eb4fc8499cc1b0cf4438e89ecbdb103cdc84361
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-03 09:10:54 -0800

    remove unused fields from SpaceData

--- src/model/SpaceData.js
+++ src/model/SpaceData.js
@@ -3,15 +3,13 @@ import Space from "../components/Space"
 export default class SpaceData {
   static ROW_COUNT = 8
 
-  constructor(column, row, section, owner, image, edgeClass) {
+  constructor(column, row, section, owner) {
     this.id = this.column * SpaceData.ROW_COUNT + this.row
     this.column = column
     this.row = row
     this.occupant = null // null===empty space, else PieceData
     this.section = section
     this.owner = owner ? owner : null // null===middle section, else PlayerData
-    this.image = image
-    this.edgeClass = edgeClass
   }
 
   get coords() {
