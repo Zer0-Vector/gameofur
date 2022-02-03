896cce52d542fa7829ca84abeae77ebb171de25e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-03 09:22:39 -0800

    add comments to AppInitializer

--- src/AppInitializer.js
+++ src/AppInitializer.js
@@ -4,9 +4,12 @@ import SpaceData from './model/SpaceData'
 
 export default class AppInitializer {
   static initGameContext() {
+    // init players
     const p1 = new PlayerData(1)
     const p2 = new PlayerData(2)
     const players = [p1,p2]
+    
+    // init pieces
     const pieceCount = 7;
     var pieces = new Array(pieceCount * players.length)
     for (var i = 0; i < players.length; i++) {
@@ -15,16 +18,18 @@ export default class AppInitializer {
       }
     }
 
+    // init spaces
     const rowCount = 8;
     const columnCount = 3; 
     const gapCount = 4;
     const bagCount = 2;
     var spaces = new Array(columnCount * rowCount - gapCount + bagCount)
-    // row -1 is where the bags live
-    // spaces.push(new SpaceData(0, -1, 'start', p1))
-    // spaces.push(new SpaceData(2, -1, 'start', p2))
-    // spaces.push(new SpaceData(0, -2, 'finish', p1))
-    // spaces.push(new SpaceData(2, -2, 'finish', p2))
+    // row -1 is where the starting areas live
+    spaces.push(new SpaceData(0, -1, 'start', p1))
+    spaces.push(new SpaceData(2, -1, 'start', p2))
+    // row -2 is where the finish areas live
+    spaces.push(new SpaceData(0, -2, 'finish', p1))
+    spaces.push(new SpaceData(2, -2, 'finish', p2))
 
     for (var c = 0; c < columnCount; c++) {
       for (var r = 0; r < rowCount; r++) {
