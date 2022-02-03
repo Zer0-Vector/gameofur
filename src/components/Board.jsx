90c9f1bc2fd165a90b1c6c65010bd380c3975a31
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-03 09:07:16 -0800

    update Board to grid layout

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,38 +1,34 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import './Board.css'
 import Space from './Space'
-import Column from '../containers/Column'
-import GameContext from '../model/GameContext'
-import Gap from './Gap'
-import SpaceDropContainer from './SpaceDropContainer'
 
 export default function Board() {
-  const [gameContext,] = useContext(GameContext)
-
-  const spaces = new Array(3)
-  for (var i = 0; i < 3; i++) {
-    const index = i;
-    spaces[i] = (()=>
-      gameContext.spaces
-          .filter(item => item.column === index && item.row >= 0)
-          .sort((a, b)=> a.row - b.row)
-          .map(item => <SpaceDropContainer key={item.coords}><Space {...item} /></SpaceDropContainer>)
-    )()
-  }
-  spaces[0].splice(4, 0, 
-    <Gap edgeClass='right top' key='gap0' />,
-    <Gap edgeClass='right bottom' key='gap1' />
-  )
-  spaces[2].splice(4, 0,
-    <Gap edgeClass='top left' key='gap2' />,
-    <Gap edgeClass='bottom left' key='gap3' />
-  )
-
-  const wrapWithColumn = (space, index) => <Column key={`col${index}`}>{space}</Column>
-
   return (
     <div className='board'>
-      {spaces.map(wrapWithColumn)}
+      <Space image='rosette' locationClass='r0 c0' />
+      <Space image='eyes1' locationClass='r1 c0' />
+      <Space image='bigfivedots' locationClass='r2 c0' />
+      <Space image='eyes0' locationClass='r3 c0' />
+      <div className='cutout-left' />
+      <Space image='rosette' locationClass='r6 c0' />
+      <Space image='smallfivedots' locationClass='r7 c0' />
+
+      <Space image='twelvedots' locationClass='r0 c1' />
+      <Space image='bigfivedots' locationClass='r1 c1' />
+      <Space image='fourfivedots' locationClass='r2 c1' />
+      <Space image='rosette' locationClass='r3 c1' />
+      <Space image='bigfivedots' locationClass='r4 c1' />
+      <Space image='fourfivedots' locationClass='r5 c1' />
+      <Space image='eyes0' locationClass='r6 c1' />
+      <Space image='bigfivedots' locationClass='r7 c1' />
+
+      <Space image='rosette' locationClass='r0 c2' />
+      <Space image='eyes1' locationClass='r1 c2' />
+      <Space image='bigfivedots' locationClass='r2 c2' />
+      <Space image='eyes0' locationClass='r3 c2' />
+      <div className='cutout-right' />
+      <Space image='rosette' locationClass='r6 c2' />
+      <Space image='smallfivedots' locationClass='r7 c2' />
     </div>
   )
 }
