4425fa38722e594d60ab594034ecb4abaab03fb7
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-03 09:12:43 -0800

    Simplify App.jsx by extracting common code into other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,15 @@
 import './App.css'
 import Game from './components/Game'
 import GameContext from './model/GameContext'
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import AppInitializer from './AppInitializer'
+import Controller from './Controller'
 
 function App() {
 
   const [context, setContext] = useState(AppInitializer.initGameContext())
 
-    const selectPiece = useCallback((pieceId) => {
-      console.log("updating context: selectPiece: ", pieceId)
-      setContext({
-        ...context,
-        pieces: context.pieces.map(p => 
-          p.id===pieceId 
-          ? {...p, selected: true }
-          : {...p, selected: false }),
-        selectedPiece: pieceId
-      })
-    })
-
-    const movePiece = useCallback((pieceId, spaceId) => {
-      console.log("Moving ", pieceId, " to ", spaceId)
-      var occupant = null
-      const pieces = context.pieces.map(p => {
-        if (p.id === pieceId) {
-          occupant = { ...p, selected: false, location: spaceId } // deselect on drop
-          return occupant
-        } else {
-          return p;
-        }
-      })
-      if (occupant !== null) {
-        setContext({
-          ...context,
-          pieces: pieces,
-          spaces: context.spaces.map(s => (
-            s.id === spaceId
-            ? { ...s, occupant: occupant }
-            : s
-          ))
-        })
-      } else {
-        console.error("Could not find piece to move: pieceId=", pieceId, ", spaceId=", spaceId)
-      }
-    })
-
-    const controller = {
-      selectPiece: selectPiece,
-      movePiece: movePiece,
-    }
+  const controller = Controller(context, setContext)
 
   return (
     <div className="app">
