3de259bad5fb9c3b7bcd07e56e2e8bae439a0d9e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-06 17:49:59 -0800

    Use space data from props to render spaces instead of hardcoding

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,34 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import GameContext from '../model/GameContext'
 import './Board.css'
 import Space from './Space'
 
-export default function Board() {
+export default function Board({ spaces }) {
+
+  const renderedSpaces = []
+  spaces.forEach((val) => {
+    if (val.row < 0) return
+    renderedSpaces.push(<Space spaceData={val} key={`space${val.id}`} />)
+  })
+
   return (
     <div className='board'>
-      <Space image='rosette' locationClass='r0 c0' />
-      <Space image='eyes1' locationClass='r1 c0' />
-      <Space image='bigfivedots' locationClass='r2 c0' />
-      <Space image='eyes0' locationClass='r3 c0' />
-      <div className='cutout-left' />
-      <Space image='rosette' locationClass='r6 c0' />
-      <Space image='smallfivedots' locationClass='r7 c0' />
-
-      <Space image='twelvedots' locationClass='r0 c1' />
-      <Space image='bigfivedots' locationClass='r1 c1' />
-      <Space image='fourfivedots' locationClass='r2 c1' />
-      <Space image='rosette' locationClass='r3 c1' />
-      <Space image='bigfivedots' locationClass='r4 c1' />
-      <Space image='fourfivedots' locationClass='r5 c1' />
-      <Space image='eyes0' locationClass='r6 c1' />
-      <Space image='bigfivedots' locationClass='r7 c1' />
-
-      <Space image='rosette' locationClass='r0 c2' />
-      <Space image='eyes1' locationClass='r1 c2' />
-      <Space image='bigfivedots' locationClass='r2 c2' />
-      <Space image='eyes0' locationClass='r3 c2' />
-      <div className='cutout-right' />
-      <Space image='rosette' locationClass='r6 c2' />
-      <Space image='smallfivedots' locationClass='r7 c2' />
+      {renderedSpaces}
     </div>
   )
 }
