bfacd89fdc135c8b1340bbb1bdb18ec46d06ee3e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-06 18:02:01 -0800

    Update player area to have finish box and dice box

--- src/components/PlayerArea.jsx
+++ src/components/PlayerArea.jsx
@@ -2,15 +2,20 @@ import React from "react"
 import './PlayerArea.css'
 import NameBox from './NameBox'
 import PiecesBox from './PiecesBox'
+import DiceBox from "./DiceBox"
 
 export default class PlayerArea extends React.Component {
 
   render() {
-    const { player, pieces } = this.props
+    const { player, pieces, startData, finishData } = this.props
     return (
       <div className={'player-area ' + player.className}>
         <NameBox player={player} />
-        <PiecesBox pieces={pieces} player={player} />
+        <div>
+          <PiecesBox pieces={pieces} player={player} spaceData={startData} />
+          <PiecesBox pieces={pieces} player={player} spaceData={finishData} />
+        </div>
+        <DiceBox ownerId={player.number}/>
       </div>
     )
   }
