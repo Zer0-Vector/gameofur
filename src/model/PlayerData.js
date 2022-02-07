183d290f4e6dd77460a068bae605693bd0022373
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-06 17:56:54 -0800

    clean up PlayerData class

--- src/model/PlayerData.js
+++ src/model/PlayerData.js
@@ -1,17 +1,20 @@
-class PlayerData {
+export default class PlayerData {
 
-  constructor(number, name, avatarIdSuffix) {
+  static PLAYER_COUNT = 2
+
+  constructor(number, colId, name, avatarIdSuffix) {
     this.number = number
     this.name = name ? name : "Player " + number
     this.avatarIdSuffix = avatarIdSuffix ? avatarIdSuffix : 0
+    this.colId = colId
   }
 
   get className() {
     return 'player' + this.number
   }
   
-  avatarId = () => this.name.replace(/\s+/, '') + this.avatarIdSuffix
+  get avatarId() {
+    return this.name.replace(/\s+/, '') + this.avatarIdSuffix
+  }
 
 }
-
-export default PlayerData
