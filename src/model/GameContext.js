3e6bc4ee4cba2c33c5760dd349345b20a5ac0a92
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-06 17:55:41 -0800

    update GameContext to use Maps. Introduce controller context.

--- src/model/GameContext.js
+++ src/model/GameContext.js
@@ -1,10 +1,18 @@
 import React from "react"
 
-const GameContext = React.createContext({
-  players: [], // PlayerData[]
-  pieces: [], // PieceData[]
-  spaces: [], // SpaceData[]
-  selectedPiece: null,
+export const GameState = React.createContext({
+  players: new Map(), // Map<id, PlayerData>
+  pieces: new Map(), // Map<id, PieceData>
+  spaces: new Map(), // Map<id, SpaceData>
+  gamePhase: null, // GamePhase
+  turnPhase: null, // TurnPhase
+  currentPlayerId: 1, // always 1 or 2; Use turnPhase
+  diceFaces: [],
 })
 
-export default GameContext
+export const GameController = React.createContext({
+  selectPiece: (pieceId)=>{},
+  movePiece: (destSpaceId)=>{},
+  rollDice: () => {},
+})
+
