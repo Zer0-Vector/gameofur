a3230215f217951e02a415b837274f810d98464d
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-02-11 15:29:03 -0800

    Make finish box behave a bit like a space

--- src/components/PiecesBox.jsx
+++ src/components/PiecesBox.jsx
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useContext } from "react"
 import './PiecesBox.css'
 import Piece from "./Piece"
+import { GameController } from "../model/GameContext"
 
 
 export default function PiecesBox({ spaceData, pieces, player }) {
+  const controller = useContext(GameController)
+
+
+  const onClick = () => {
+    // TODO can't finish more than one piece because it thinks it's occupied
+    controller.movePiece(spaceData.id)
+  }
+
+  const classes = ['pieces-box', spaceData.className]
+  const isLegal = controller.isLegalMove(spaceData.id)
+  if (isLegal) {
+    console.log("FINISH IS LEGAL")
+    classes.push('legal-move')
+  }
+
   return (
-    <div className={"pieces-box " + spaceData.className}>
+    <div className={classes.join(' ')} onClick={isLegal ? onClick : ()=>{}}>
       {
         pieces
           .filter(p => p.locationId === spaceData.id)
