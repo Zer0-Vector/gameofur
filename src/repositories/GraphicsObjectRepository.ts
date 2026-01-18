955d59269f96e6bfacb0e3e7c77c6dac7a8c7bc8
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2026-01-18 15:00:15 -0500

    refactor(GraphicsObjectRepository): remove unneeded type parameter

--- src/repositories/GraphicsObjectRepository.ts
+++ src/repositories/GraphicsObjectRepository.ts
@@ -1,9 +1,8 @@
-import type { Object3D } from "three";
 import { Repository } from "./Repository";
 import type { GraphicsObject } from "@/graphics/GraphicsObject";
 /**
  * Repository for managing graphics objects.
  */
-export class GraphicsObjectRepository extends Repository<GraphicsObject<Object3D>> {
+export class GraphicsObjectRepository extends Repository<GraphicsObject> {
 
 }
