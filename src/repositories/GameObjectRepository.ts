936c34e802c98ef6e12472298b5de782ce9dd90e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2026-01-21 12:25:41 -0500

    refactor: remove unused repository classes

--- src/repositories/GameObjectRepository.ts
+++
@@ -1,6 +0,0 @@
-import type { GameObject } from "@/objects";
-import { BasicRepository } from "./BasicRepository";
-
-export class GameObjectRepository extends BasicRepository<GameObject<any>> {
-
-}
--- src/repositories/GraphicsObjectRepository.ts
+++
@@ -1,8 +0,0 @@
-import { BasicRepository } from "./BasicRepository";
-import type { GraphicsObject } from "@/graphics/GraphicsObject";
-/**
- * Repository for managing graphics objects.
- */
-export class GraphicsObjectRepository extends BasicRepository<GraphicsObject> {
-
-}
